2703a8154e64ba79e50d5a1623eb7d395af51a76
TCRONEB HACKX <thelastcroneb@gmail.com>
2025-04-24 20:26:46 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import FormData from 'form-data';
 // Keep the repository private and add your details. OK
 const CONFIG = {
   BOT_TOKEN: '7632867723:AAHTIl6E4kweDPtiujkQzRYKxrTfJGmZpbQ',
-  PUBLICKEY: 'public_q4N61YFX+/wDC2nUNa+5QOSx+4Y=',
+  API_KEY: '1d5a015a2eba38ffc04319de665e2134',
   VERCEL_URL: 'https://imgbb-image-uploader.vercel.app'
 };
 
